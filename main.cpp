d0bd8bb5023731c9637b2881f4a0f4c953d5bbc3
justusBe <justusbeasley@Justuss-MacBook-Air.local>
2024-12-10 11:50:41 -0600

    adds some functions to so the prompts are useable

--- main.cpp
+++ main.cpp
@@ -11,17 +11,51 @@
  */
 
 #include <iostream>
+#include <cstring>
+#include <fstream>
+
+vector<string> COLLECTION; // a default collection list to start the user off
 
 
 using namespace std;
+
+void newEntry() //This will prompt the user to add a new Funko pop to their collection
+{
+    cout << 
+    mission();
+}
+
+void viewList() // This will prompt the user to view their list
+{
+
+    mission();
+}
+
+void createList() // this will create a new list for the user to add to
+{
+    string listName;
+    cout << "List Name: " << endl;
+    cin >> listName;
+    ofstream outFile(listName);
+    ofstream  ListNames("LISTS");
+    if(ListNames.is_open())
+    {
+        listNames << "\n" << listName; // add the new list to the availible lists
+        listNames.close(); // close the file
+    }
+    
+
+    mission();
+
+}
 
 string mission() //find out what the users intentions are
 {
     int decision;
     cout << "Please Enter a Number : " << endl
-        << "(1) ADD NEW" << endl
+        << "(1) ADD NEW ENTRY" << endl
         << "(2) VIEW LIST" << endl
-        << "(3) " << endl 
+        << "(3) CREATE LIST" << endl 
         << "(4) " << endl
         << "(5) " << endl;
 
@@ -29,6 +63,7 @@ string mission() //find out what the users intentions are
     switch(decision)
     {
         case 1:
+            newEntry();
         case 2:
         case 3:
         case 4:
